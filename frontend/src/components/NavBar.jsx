26e4d1c4face637edce4f6be5dd5e24c6190c7d8
Crab-Prog <ophelie.coudert@gmail.com>
2022-04-13 16:32:51 +0200

    I changed the import to link the css at the navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,21 +1,17 @@
 import React from "react";
-import navbar from "../assets/navbar.css"
+import "../assets/navbar.css";
 
-const NavBar = () => {
-	return (
-      <div>
-        <nav className="navigation">
-          <ul className="navigation-bar">
-            <li className="navigation-name">
-              Random Cocktail
-            </li>
-            <li>
-              Popular Cocktail
-            </li>
-          </ul>
-        </nav>
-      </div>
-	);
-};
+function NavBar() {
+  return (
+    <div>
+      <nav className="navigation">
+        <ul className="navigation-bar">
+          <li className="navigation-name">Random Cocktail</li>
+          <li>Popular Cocktail</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
 
 export default NavBar;
