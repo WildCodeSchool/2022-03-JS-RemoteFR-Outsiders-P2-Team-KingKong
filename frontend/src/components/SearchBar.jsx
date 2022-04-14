2915b85638b2321a12f4cd8235cd1e23697f04c0
Crab-Prog <ophelie.coudert@gmail.com>
2022-04-14 16:00:21 +0200

    You can type one of the exemple in the search bar and it will select only this one, I used names as exemples

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import searchlogo from "../assets/magnifying-glass-search-svgrepo-com.svg";
 import "../assets/searchbar.css";
 
-function SearchBar() {
+function SearchBar(props) {
+  const { searchValue, handleSearchValue } = props;
+  console.log(props);
   return (
     <div className="full-searchbar">
       <div className="searchbar">
         <input
+        value={searchValue}
           type="text"
-          name="name"
           placeholder="Mojito..."
           className="input"
+          onChange={(e) => handleSearchValue(e.target.value)}
         />
         <button type="button" className="btn">
           <img src={searchlogo} alt="" className="image-logo" />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,25 @@
-import Counter from "@components/Counter";
-import logo from "@assets/logo.svg";
+import React, {useState} from "react";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState("");
+  const [cocktails, setCocktail] = useState([
+    "Margarita",
+    "Blue Margarita",
+    "Mojito",
+    "Old Fashioned",
+    "Whiskey Sour"
+]);
+
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
-    </header>
+    <div>
+      <header>
+        <SearchBar searchValue={searchValue} handleSearchValue={setSearchValue} />
+      </header>
+      {cocktails.filter((cocktail) => cocktail.includes(searchValue))
+      .map((cocktail) => (
+        <p>{cocktail}</p>
+      ))}
+    </div>
   );
 }
