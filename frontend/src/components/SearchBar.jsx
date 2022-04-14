79aa1b4f5aa09b49285704b139706cf30f5d4934
Crab-Prog <ophelie.coudert@gmail.com>
2022-04-14 15:02:29 +0200

    Responsive, the searchbar is going on the right when you are on a screen above 768px

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import searchlogo from "../assets/magnifying-glass-search-svgrepo-com.svg"
-import "../assets/searchbar.css"
+import searchlogo from "../assets/magnifying-glass-search-svgrepo-com.svg";
+import "../assets/searchbar.css";
 
-function SearchBar(props) {
-    const { searchValue, handleSearchValue } = props;
-    return (
-        <div className="full-searchbar">
-            <div className="searchbar">
-                <input
-                    value={searchValue}
-                    type="text"
-                    placeholder="Mojito..."
-                    className="input"
-                    onChange={(e) => handleSearchValue(e.target.value)}
-                />
-                <button type="button" className="btn"><img src={searchlogo} alt="" className="image-logo"/></button>
-            </div>
-            </div>
-    );
+function SearchBar() {
+  return (
+    <div className="full-searchbar">
+      <div className="searchbar">
+        <input
+          type="text"
+          name="name"
+          placeholder="Mojito..."
+          className="input"
+        />
+        <button type="button" className="btn">
+          <img src={searchlogo} alt="" className="image-logo" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
