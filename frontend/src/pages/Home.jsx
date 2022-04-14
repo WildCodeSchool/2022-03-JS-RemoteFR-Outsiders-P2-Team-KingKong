7b020b0e2c11de8605f1149174a552c9aabd6e01
froustex <stephane-feroussier_student2022@wilder.school>
2022-04-14 16:38:48 +0200

    footer version first sprint  HP_04_footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Counter from "@components/Counter";
 import logo from "@assets/logo.svg";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
@@ -31,6 +32,7 @@ export default function Home() {
           Vite Docs
         </a>
       </p>
+      <Footer />
     </header>
   );
 }
