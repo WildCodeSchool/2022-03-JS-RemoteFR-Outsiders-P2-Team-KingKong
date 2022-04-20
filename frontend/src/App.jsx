1e865c9009a8f3c92ece5a6c7b3a3a19fb9136a0
Valerie <valerie.ielsch@gmail.com>
2022-04-20 15:27:13 +0200

    build: move div nav from App.jsx to NavBar.jsx with Link to

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Random from '@pages/Random';
-import PopularCocktail from '@pages/PopularCocktail';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "@components/NavBar";
+import Random from "@pages/Random";
+import PopularCocktail from "@pages/PopularCocktail";
 import Banner from "@components/Banner";
 import Home from "@pages/Home";
 import "@assets/navbar.css";
@@ -11,19 +12,7 @@ function App() {
       <Banner />
       <Router>
         <div>
-          <nav className="navigation">
-            <ul className="navigation-bar">
-              <li className="navigation-name">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="navigation-name">
-                <Link to="/random">Random Cocktail</Link>
-              </li>
-              <li className="navigation-name">
-                <Link to="/popularcocktail">Popular Cocktail</Link>
-              </li>
-            </ul>
-          </nav>
+          <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/random" element={<Random />} />
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "../assets/navbar.css";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <div>
       <nav className="navigation">
         <ul className="navigation-bar">
-          <li className="navigation-name">Random Cocktail</li>
-          <li>Popular Cocktail</li>
+          <li className="navigation-name">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navigation-name">
+            <Link to="/random">Random Cocktail</Link>
+          </li>
+          <li className="navigation-name">
+            <Link to="/popularcocktail">Popular Cocktail</Link>
+          </li>
         </ul>
       </nav>
     </div>
