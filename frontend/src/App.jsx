d809de25227f611fc45e53d174e2ea541bf220b0
Crab-Prog <ophelie.coudert@gmail.com>
2022-04-20 17:10:04 +0200

    eslint fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,19 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
 import Random from "@pages/Random";
 import PopularCocktail from "@pages/PopularCocktail";
 import Banner from "@components/Banner";
 import Home from "@pages/Home";
-import SearchBar from "@components/SearchBar";
 import "@assets/navbar.css";
+import NavBar from "@components/NavBar";
 
 function App() {
-  const [searchValue, setSearchValue] = useState("");
-  const [cocktails] = useState(["Margarita", "Mojito"]);
   return (
     <div>
       <Banner />
       <Router>
         <div>
-          <div className="full-navigation">
-            <nav className="navigation">
-              <ul className="navigation-bar">
-                <li className="navigation-name">
-                  <Link to="/">Home</Link>
-                </li>
-                <li className="navigation-name">
-                  <Link to="/random">Random Cocktail</Link>
-                </li>
-                <li className="navigation-name">
-                  <Link to="/popularcocktail">Popular Cocktail</Link>
-                </li>
-              </ul>
-              <SearchBar
-                searchValue={searchValue}
-                handleSearchValue={setSearchValue}
-              />
-              {cocktails
-                .filter((cocktail) => cocktail.includes(searchValue))
-                .map((cocktail) => (
-                  <p>{cocktail}</p>
-                ))}
-            </nav>
-          </div>
+          <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/random" element={<Random />} />
