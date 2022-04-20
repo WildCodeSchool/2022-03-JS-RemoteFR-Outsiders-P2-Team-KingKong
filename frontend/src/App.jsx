0be45c3605993baf0775c297bc898152c98168ef
Crab-Prog <ophelie.coudert@gmail.com>
2022-04-20 16:18:09 +0200

    I added the search into the navbar and added a flex, I deleted the logo.svg, and added some css to ne navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,44 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Random from '@pages/Random';
-import PopularCocktail from '@pages/PopularCocktail';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import React, { useState } from "react";
+import Random from "@pages/Random";
+import PopularCocktail from "@pages/PopularCocktail";
 import Banner from "@components/Banner";
 import Home from "@pages/Home";
+import SearchBar from "@components/SearchBar";
 import "@assets/navbar.css";
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const [cocktails] = useState(["Margarita", "Mojito"]);
   return (
     <div>
       <Banner />
       <Router>
         <div>
-          <nav className="navigation">
-            <ul className="navigation-bar">
-              <li className="navigation-name">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="navigation-name">
-                <Link to="/random">Random Cocktail</Link>
-              </li>
-              <li className="navigation-name">
-                <Link to="/popularcocktail">Popular Cocktail</Link>
-              </li>
-            </ul>
-          </nav>
+          <div className="full-navigation">
+            <nav className="navigation">
+              <ul className="navigation-bar">
+                <li className="navigation-name">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="navigation-name">
+                  <Link to="/random">Random Cocktail</Link>
+                </li>
+                <li className="navigation-name">
+                  <Link to="/popularcocktail">Popular Cocktail</Link>
+                </li>
+              </ul>
+              <SearchBar
+                searchValue={searchValue}
+                handleSearchValue={setSearchValue}
+              />
+              {cocktails
+                .filter((cocktail) => cocktail.includes(searchValue))
+                .map((cocktail) => (
+                  <p>{cocktail}</p>
+                ))}
+            </nav>
+          </div>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/random" element={<Random />} />
