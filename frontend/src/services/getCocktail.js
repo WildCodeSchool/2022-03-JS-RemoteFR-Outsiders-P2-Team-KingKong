4d5a43d2c4b9324f34cb6de8dd628675cccf96c3
o.pochic@gmail.com <o.pochic@gmail.com>
2022-04-27 11:46:23 +0200

    HP_02_API ajout ASYNC/AWAIT avec juste la fonction GetByNamegetCocktailByName - du 2202-04-27 par olivier

--- frontend/src/services/getCocktail.js
+++ frontend/src/services/getCocktail.js
@@ -1,15 +1,89 @@
 import axios from "axios";
 
-function getCocktailByName(name) {
-  // const API = "https://www.thecocktaildb.com/api.php";
+// 2 problemes : impossible de return et random fonctionne pas
+export default function getCocktailByName(name, number) {
   const test = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`;
+  const cocktailList = [];
+  const cocktail = {};
   axios
     .get(test)
     .then((response) => response.data)
     .then((data) => {
-      console.warn(data[0]);
-      // for (let i = 0; i < number; i++) {} */
+      for (let i = 0; i < number; i++) {
+        (cocktail.id = data.drinks[i].idDrink),
+          (cocktail.title = data.drinks[i].strDrink),
+          (cocktail.image = data.drinks[i].strDrinkThumb),
+          (cocktail.instructions = data.drinks[i].strInstructions),
+          (cocktail.ingredients = getIngredientsToArray(data.drinks[i])),
+          (cocktail.quantity = getMeasuresToArray(data.drinks[i])),
+          cocktailList.push(cocktail);
+      }
+      console.log(cocktailList);
     });
 }
 
-export default getCocktailByName;
+function getCocktailByIngredient(ingredient, number) {
+  const test = `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${ingredient}`;
+  const cocktailList = [];
+  const cocktail = {};
+
+  axios
+    .get(test)
+    .then((response) => response.data)
+    .then((data) => {
+      for (let i = 0; i < number; i++) {
+        (cocktail.id = data.drinks[i].idDrink),
+          (cocktail.title = data.drinks[i].strDrink),
+          (cocktail.image = data.drinks[i].strDrinkThumb),
+          (cocktail.instructions = data.drinks[i].strInstructions),
+          (cocktail.ingredients = getIngredientsToArray(data.drinks[i])),
+          (cocktail.quantity = getMeasuresToArray(data.drinks[i])),
+          cocktailList.push(cocktail);
+      }
+      console.log(cocktailList);
+    });
+}
+
+function getIngredientsToArray(list) {
+  let ingredientsList = [
+    list.strIngredient1,
+    list.strIngredient2,
+    list.strIngredient3,
+    list.strIngredient4,
+    list.strIngredient5,
+    list.strIngredient6,
+    list.strIngredient7,
+    list.strIngredient8,
+    list.strIngredient9,
+    list.strIngredient10,
+    list.strIngredient11,
+    list.strIngredient12,
+    list.strIngredient13,
+    list.strIngredient14,
+    list.strIngredient15,
+  ];
+  return ingredientsList;
+}
+
+function getMeasuresToArray(list) {
+  let measuresList = [
+    list.strMeasure1,
+    list.strMeasure2,
+    list.strMeasure3,
+    list.strMeasure4,
+    list.strMeasure5,
+    list.strMeasure6,
+    list.strMeasure7,
+    list.strMeasure8,
+    list.strMeasure9,
+    list.strMeasure10,
+    list.strMeasure11,
+    list.strMeasure12,
+    list.strMeasure13,
+    list.strMeasure14,
+    list.strMeasure15,
+  ];
+  return measuresList;
+}
+
+export { getCocktailByName, getCocktailByIngredient };
