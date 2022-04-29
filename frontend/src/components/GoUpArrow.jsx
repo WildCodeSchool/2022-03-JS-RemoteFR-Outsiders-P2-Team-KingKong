b43bb795e066d8a3fbaae2f6737a7d90c2196d8e
Crab-Prog <ophelie.coudert@gmail.com>
2022-04-29 23:53:08 +0200

    Added the arrow to go up, changed the hover on the navigation, and changed the color for the like hearts.

---
+++ frontend/src/components/GoUpArrow.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "@assets/gouparrow.css";
+
+const GoUp = () => {
+  return (
+    <div className="goup-part">
+      <a href="#section-0"><img
+        src="https://www.svgrepo.com/show/409000/up.svg"
+        alt=""
+        className="goup-logo"
+      /></a>
+    </div>
+  );
+};
+
+export default GoUp;
